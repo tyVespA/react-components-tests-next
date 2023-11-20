6729682dee8dd076c1ef1b30c3c7d5bd374b0a0d
tyVespA <migliavacca.marco08@gmail.com>
2023-11-20 12:49:37 +0100

    Create and style

--- app/partOne/02-button/page.js
+++ app/partOne/02-button/page.js
@@ -1,9 +1,32 @@
 import React from "react";
+import styles from "./02-button.module.css";
+
+function Button({ children, color, border }) {
+  return (
+    <button
+      className={styles.button}
+      style={{ color, border: `5px solid ${border}` }}
+    >
+      {children}
+    </button>
+  );
+}
 
 function page() {
   return (
     <div className="exerciseContainer">
-      <em>build a button</em>
+      <em>
+        Build a button component that can set its own text content and choose
+        text/border color
+      </em>
+      <div className={styles.buttonsContainer}>
+        <Button color="darkgreen" border="lightgreen">
+          Content 1
+        </Button>
+        <Button color="darkblue" border="purple">
+          Content 2
+        </Button>
+      </div>
     </div>
   );
 }
