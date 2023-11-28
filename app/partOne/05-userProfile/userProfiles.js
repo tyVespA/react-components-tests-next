0525a05910d5c5320fc8ae63c2146973fdb16acb
tyVespA <migliavacca.marco08@gmail.com>
2023-11-28 18:07:31 +0100

    Populate array

---
+++ app/partOne/05-userProfile/userProfiles.js
@@ -0,0 +1,36 @@
+export const userProfiles = [
+  {
+    id: "marco-302",
+    imageSrc: "Ander.png",
+    imageAlt: "",
+    bannerColor: "lightblue",
+    name: "Marco",
+    joinDate: "August 3rd 2012",
+    badges: [],
+  },
+  {
+    id: "pudge-1",
+    imageSrc: "Pudge_icon.webp",
+    imageAlt: "",
+    bannerColor: "lightpink",
+    name: "Pudge",
+    joinDate: "January 21st 2002",
+    badges: [
+      { title: "Hoock Master", label: "🪝" },
+      { title: "Party Starter", label: "🎉" },
+      { title: "Pretty", label: "❤️" },
+    ],
+  },
+  {
+    id: "walter-0",
+    imageSrc: "WaltSeason2.webp",
+    imageAlt: "",
+    bannerColor: "darkgrey",
+    name: "Walter",
+    joinDate: "December 15th 1998",
+    badges: [
+      { title: "Pollo Ermanos Costumer", label: "🦃" },
+      { title: "Ice Producer", label: "❄️" },
+    ],
+  },
+];
