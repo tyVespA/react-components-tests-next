bac327e8b874905df3b3a1a6f2e482dfa524e9b2
tyVespA <migliavacca.marco08@gmail.com>
2023-11-28 18:08:25 +0100

    Display the profiles with map

--- app/partOne/05-userProfile/page.js
+++ app/partOne/05-userProfile/page.js
@@ -1,12 +1,20 @@
 import React from "react";
 import styles from "./page.module.css";
 import ProfileCard from "./ProfileCard";
+import { userProfiles } from "./userProfiles";
 
 export default function page() {
   return (
     <div className="exerciseContainer">
-      <em>Update a set of user profiles to conditionally render some badges</em>
-      <ProfileCard />
+      <em>
+        Display a set of user profiles from an array and conditionally render
+        some badges
+      </em>
+      <div className={styles.profilesContainer}>
+        {userProfiles.map((profile) => (
+          <ProfileCard key={profile.id} profile={profile} />
+        ))}
+      </div>
     </div>
   );
 }
